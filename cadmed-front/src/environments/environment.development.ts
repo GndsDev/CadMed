444edd7eca78bb0c65c1462f170518181997bb6d
GndsDev <gabrielnunesdossantoss@gmail.com>
2026-03-13 10:00:25 -0300

    Atualizando URL da Environments

--- cadmed-front/src/environments/environment.development.ts
+++ cadmed-front/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://34.31.241.110' // Aqui vai o IP do Google Cloud sem a porta 8080!
+  apiUrl: 'http://34.31.241.110:8080' // Aqui vai o IP do Google Cloud sem a porta 8080!
 };
