64261128277230ab2c106b86488013d3eb746298
GndsDev <gabrielnunesdossantoss@gmail.com>
2026-03-17 11:20:26 -0300

    rota

--- cadmed-front/src/environments/environment.development.ts
+++ cadmed-front/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080' // Backend Spring Boot em localhost:8080 (ajuste para o IP/host de produção conforme necessário)
+  apiUrl: 'http://34.31.241.110' // Backend Spring Boot em localhost:8080 (ajuste para o IP/host de produção conforme necessário)
 };
--- cadmed-front/src/environments/environment.ts
+++ cadmed-front/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080' // Backend Spring Boot em localhost:8080
+  apiUrl: 'http://34.31.241.110' // Backend Spring Boot em localhost:8080
 };
